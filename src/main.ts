42d80b48e5c77d59896b6350e746c48d8f75f1f8
iulius <btorrez@aduana.gob.bo>
2025-03-10 22:18:30 -0400

    feat: configurar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notificaciones API')
+    .setDescription('Api para casos de notificaciones unicas')
+    .setVersion('1.0.0')
+    .addTag('notificaciones')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.info("Servidor en el puerto:", process.env.PORT ?? 3000)
+  });
 }
 bootstrap();
